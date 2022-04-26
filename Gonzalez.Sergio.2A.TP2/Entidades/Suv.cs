0ec60a2311c49147ef4a17f14ea03e5b29986893
SERGIO-G <01sergiogonzalez01@gmail.com>
2022-04-26 20:49:40 -0300

    Subiendo Tp2 Resuelto

---
+++ Gonzalez.Sergio.2A.TP2/Entidades/Suv.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Suv : Vehiculo 
+    {
+        public Suv(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+        }
+        /// <summary>
+        /// SUV son 'Grande'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("\nSUV");
+            sb.AppendLine(base.Mostrar());
+            sb.Append($"TAMAÑO: {this.Tamanio}");
+            sb.Append("\n---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
