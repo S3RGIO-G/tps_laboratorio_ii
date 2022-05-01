92393891537dd5b52eaf40ac2b6a75008d9c6e18
SERGIO-G <01sergiogonzalez01@gmail.com>
2022-05-01 16:08:12 -0300

    Correcion de errores

--- Gonzalez.Sergio.2A.TP2/Entidades/Suv.cs
+++ Gonzalez.Sergio.2A.TP2/Entidades/Suv.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    public class Suv : Vehiculo 
+    public sealed class Suv : Vehiculo 
     {
         public Suv(EMarca marca, string chasis, ConsoleColor color) : base(chasis, marca, color){ }
 
